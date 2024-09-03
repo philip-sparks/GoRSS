c1a7c7889e5c6746fdc305a83fc7f9b86a6a62b8
Phil <phil.sparks.work@gmail.com>
2024-09-02 21:39:22 -0400

    P4 Authentication Changes

--- handler_user.go
+++ handler_user.go
@@ -37,6 +37,24 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	respondWithJSON(w, 200, databaseUserToUser(user))
+	respondWithJSON(w, 201, databaseUserToUser(user))
 
 }
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User) {
+	respondWithJSON(w, 200, databaseUserToUser(user))
+}
+
+// func (apiCfg *apiConfig) handlerGetPostsForUser(w http.ResponseWriter, r *http.Request, user database.User) {
+// 	posts, err := apiCfg.DB.GetPostsForUser(r.Context(), database.GetPostsForUserParams{
+// 		UserID: user.ID,
+// 		Limit:  10,
+// 	})
+
+// 	if err != nil {
+// 		respondWithError(w, 400, fmt.Sprintf("Couldn't get posts: %v", err))
+// 		return
+// 	}
+
+// 	respondWithJSON(w, 200, databasePostsToPosts(posts))
+// }
